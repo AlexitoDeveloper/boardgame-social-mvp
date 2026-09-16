290f8d56c8b5b86757de639aa470ad977269902b
ald19 <ald19@alu.ua.es>
2026-09-16 17:27:11 +0200

    feat: add profile achievements system with UI components and asset mappings

---
+++ src/components/profile/achievementAssets.ts
@@ -0,0 +1,67 @@
+import hostBronce from '../../assets/achievements/host_bronce.jpg'
+import hostPlata from '../../assets/achievements/host_plata.jpg'
+import hostOro from '../../assets/achievements/host_oro.jpg'
+import hostPlatino from '../../assets/achievements/host_platino.jpg'
+
+import winnerBronce from '../../assets/achievements/winner_bronce.jpg'
+import winnerPlata from '../../assets/achievements/winner_plata.jpg'
+import winnerOro from '../../assets/achievements/winner_oro.jpg'
+import winnerPlatino from '../../assets/achievements/winner_platino.jpg'
+
+import veteranBronce from '../../assets/achievements/veteran_bronce.jpg'
+import veteranPlata from '../../assets/achievements/veteran_plata.jpg'
+import veteranOro from '../../assets/achievements/veteran_oro.jpg'
+import veteranPlatino from '../../assets/achievements/veteran_platino.jpg'
+
+import reliableBronce from '../../assets/achievements/reliable_bronce.jpg'
+import reliablePlata from '../../assets/achievements/reliable_plata.jpg'
+import reliableOro from '../../assets/achievements/reliable_oro.jpg'
+import reliablePlatino from '../../assets/achievements/reliable_platino.jpg'
+
+import criticBronce from '../../assets/achievements/critic_bronce.jpg'
+import criticPlata from '../../assets/achievements/critic_plata.jpg'
+import criticOro from '../../assets/achievements/critic_oro.jpg'
+import criticPlatino from '../../assets/achievements/critic_platino.jpg'
+
+import legendImg from '../../assets/titles/legend.png'
+import conquerorImg from '../../assets/titles/conqueror.jpg'
+import eternalImg from '../../assets/titles/eternal.jpg'
+import ghostImg from '../../assets/titles/ghost.jpg'
+import socialImg from '../../assets/titles/social.jpg'
+import rookieImg from '../../assets/titles/rookie.png'
+
+export const ACHIEVEMENT_TIER_ASSETS: Record<string, string> = {
+  host_bronce: hostBronce,
+  host_plata: hostPlata,
+  host_oro: hostOro,
+  host_platino: hostPlatino,
+
+  winner_bronce: winnerBronce,
+  winner_plata: winnerPlata,
+  winner_oro: winnerOro,
+  winner_platino: winnerPlatino,
+
+  veteran_bronce: veteranBronce,
+  veteran_plata: veteranPlata,
+  veteran_oro: veteranOro,
+  veteran_platino: veteranPlatino,
+
+  reliable_bronce: reliableBronce,
+  reliable_plata: reliablePlata,
+  reliable_oro: reliableOro,
+  reliable_platino: reliablePlatino,
+
+  critic_bronce: criticBronce,
+  critic_plata: criticPlata,
+  critic_oro: criticOro,
+  critic_platino: criticPlatino,
+}
+
+export const TITLE_IMAGES: Record<string, string> = {
+  legend: legendImg,
+  conqueror: conquerorImg,
+  eternal: eternalImg,
+  ghost: ghostImg,
+  social: socialImg,
+  rookie: rookieImg,
+}
